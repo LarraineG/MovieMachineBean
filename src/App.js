1193dc92e8baa581004e37d3f4c2f1f58f50e0a3
chlokins <cnpolansky@gmail.com>
2020-10-07 13:40:22 -0500

    added App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+    return (
+        <div className="">
+            <Layout>
+                <Header className="" title="Title" scroll>
+                    <Navigation>
+                        <Link to="/home">Home</Link>
+                        <Link to="/login">Login</Link>
+                        <Link to="/mylist">My List</Link>
+                        <Link to="/moviespage">Movies Page</Link>
+                        <Link to="/adminpage">Callum Page</Link>
+                        <Link to="/adminpage/id:1">Angela Page</Link>
+                        <Link to="/adminpage/id:2">Larraine Page</Link>
+                        <Link to="/adminpage/id:3">Chloe Page</Link>
+                    </Navigation>
+                </Header>
+            </Layout>
+        </div>
+    );
+}
+
+export default App;
