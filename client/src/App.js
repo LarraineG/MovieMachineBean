8d905f24d4e6956a30855380338f7faf1500a5ad
chlokins <cnpolansky@gmail.com>
2020-10-09 10:16:12 -0500

    pulling angelas work into my branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
-import Navigation from './components/NavBar' 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home'
+import MoviePage from './pages/MoviePage'
+import UserList from './pages/UserList'
+import Login from './pages/Login'
+import Calum from './pages/Calum'
+import Chloe from './pages/Chloe'
+import Larraine from './pages/Larraine'
+import Angela from './pages/Angela'
+
 function App() {
     return (
-        <div className="">
-            {/* <Layout> */}
-                {/* <Header className="" title="Title" scroll> */}
-                    <Navigation>
-                        <Link to="/home">Home</Link>
-                        <Link to="/login">Login</Link>
-                        <Link to="/mylist">My List</Link>
-                        <Link to="/moviespage">Movies Page</Link>
-                        <Link to="/adminpage/id:1">Callum Page</Link>
-                        <Link to="/adminpage/id:2">Angela Page</Link>
-                        <Link to="/adminpage/id:3">Larraine Page</Link>
-                        <Link to="/adminpage/id:4">Chloe Page</Link>
-                    </Navigation>
-                {/* </Header> */}
-            {/* </Layout> */}
+        <Router>
+        <div>
+        <Route exact path={["/Home", "/"]} component={Home}/>
+        <Route exact path="/MoviePage" component={MoviePage}/>
+        <Route exact path="/UserList" component={UserList}/>
+        <Route exact path="/Login" component={Login}/>
+        <Route exact path="/Calum" component={Calum}/>
+        <Route exact path="/Chloe" component={Chloe}/>
+        <Route exact path="/Larraine" component={Larraine}/>
+        <Route exact path="/Angela" component={Angela}/>
         </div>
+        </Router>
+
+
+
     );
 }
 export default App;
---
+++ client/src/pages/UserList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../components/NavBar'
+
+const UserList = (props) => {
+    return (
+        <div>
+        
+        <Navigation />
+    
+        </div>
+    );
+}
+
+
+export default UserList;
