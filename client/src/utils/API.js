7edf81721a813465b8708ee04995333eee73a381
Calum Held <calumheld@gmail.com>
2020-10-13 11:10:00 -0500

    added utelly call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -19,7 +19,7 @@ const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
     //     console.log(moviesarray);
     // });
     const idCall=function(id){
-        var queryURL="https://api.themoviedb.org/3/movie/"+id+"?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&append_to_response=credits"
+        var queryURL="https://api.themoviedb.org/3/movie/"+id+"?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&append_to_response=credits,external_ids"
         $.ajax({
             url:queryURL,
             method:"GET"
@@ -43,9 +43,29 @@ const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
                 posterUrl:"https://image.tmdb.org/t/p/w600_and_h900_bestv2"+response.poster_path,
                 id:id
             }
-            console.log(movObj);
+            return(movObj);
         });
     }
+    const utellyCall=function(id){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id="+id+"&source=tmdb",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+                "x-rapidapi-key": "9b41398bccmsh054a4c1235dff30p1c1836jsn95542e375cf5"
+            }
+        }
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            var utellyobj=response.collection.locations;
+            return(utellyobj);
+        });
+
+    }
     module.exports={
-        idCall:idCall
+        idCall:idCall,
+        utellyCall:utellyCall
     };
