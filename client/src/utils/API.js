340de5793cf3b6c7f909e82e68a39c9dc2b2d74a
Calum Held <calumheld@gmail.com>
2020-10-13 11:33:25 -0500

    added api call for a search

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,24 @@
 const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
-    // var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&query=marvel&page=1&include_adult=false";
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response){
-    //     console.log(response);
-    //     var moviesarray=response.results.map(arrfunc);
-    //     function arrfunc(mov){
-    //         var movobj={
-    //             title:mov.title,
-    //             date:mov.release_date,
-    //             summary:mov.overview,
-    //             id:mov.id,
-    //             poster:"https://image.tmdb.org/t/p/w200"+mov.poster_path
-    //         }
-    //             return(movobj);
-    //     }
-    //     console.log(moviesarray);
-    // });
+    const searchCall=function(search){var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&query="+search+"&page=1&include_adult=false";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response){
+                console.log(response);
+                var moviesarray=response.results.map(arrfunc);
+                function arrfunc(mov){
+                    var movobj={
+                        title:mov.title,
+                        date:mov.release_date,
+                        summary:mov.overview,
+                        id:mov.id,
+                        poster:"https://image.tmdb.org/t/p/w200"+mov.poster_path
+                    }
+                        return(movobj);
+                }
+                console.log(moviesarray);
+            });}
+        
     const idCall=function(id){
         var queryURL="https://api.themoviedb.org/3/movie/"+id+"?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&append_to_response=credits,external_ids"
         $.ajax({
@@ -40,6 +41,7 @@ const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
                 year:response.release_date.substring(0,4),
                 director:dirObj[0].name,
                 cast:cast,
+                summary:response.overview,
                 posterUrl:"https://image.tmdb.org/t/p/w600_and_h900_bestv2"+response.poster_path,
                 id:id
             }
@@ -68,4 +70,5 @@ const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
     module.exports={
         idCall:idCall,
         utellyCall:utellyCall
+        ,searchCall:searchCall
     };
