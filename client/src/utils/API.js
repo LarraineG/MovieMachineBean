f9cd8018ddd1428ea83edf0917d48a083f90b40e
Calum Held <calumheld@gmail.com>
2020-10-15 15:00:52 -0500

    got movie title to appear on moviepage

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,50 +3,12 @@ import axios from "axios";
 const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
     const searchCall=function(search){
     var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&query="+search+"&page=1&include_adult=false";
-            axios.get(queryURL)
-                .then(function(response){
-                console.log(response);
-                var moviesarray=response.results.map(arrfunc);
-                function arrfunc(mov){
-                    var movobj={
-                        title:mov.title,
-                        date:mov.release_date.substring(0,4),
-                        summary:mov.overview,
-                        id:mov.id,
-                        poster:"https://image.tmdb.org/t/p/w200"+mov.poster_path
-                    }
-                        return(movobj);
-                }
-                console.log(moviesarray);
-            });}
+            return(axios.get(queryURL));
+    }  
     const idCall=function(id){
         var queryURL="https://api.themoviedb.org/3/movie/"+id+"?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&append_to_response=credits,external_ids"
-        axios.get(queryURL)
-        .then(function(response){
-            console.log(response);
-            var dirObj=response.credits.crew.filter(x=> x.department=="Directing");
-            var cast="";
-            for(i=0;i<3&&i<response.credits.cast.length;i++){
-                if (cast.length>1){
-                    cast+=(", ")
-                }
-                cast+=(response.credits.cast[i].name);
-            }
-            console.log(cast)
-            
-            var movObj={
-                title:response.title,
-                year:response.release_date.substring(0,4),
-                director:dirObj[0].name,
-                cast:cast,
-                genres:response.genres,
-                runtime:response.runtime,
-                summary:response.overview,
-                posterUrl:"https://image.tmdb.org/t/p/w600_and_h900_bestv2"+response.poster_path,
-                id:id
-            }
-            return(movObj);
-        });
+        return(axios.get(queryURL));
+        
     }
     const utellyCall=function(id){
         
@@ -74,7 +36,7 @@ const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
             })
 
     }
-    module.exports={
+    export default{
         idCall:idCall,
         utellyCall:utellyCall
         ,searchCall:searchCall
