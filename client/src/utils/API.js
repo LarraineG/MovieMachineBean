6660f203b24fb522415235e6d4ab49c45aab1e0b
Calum Held <calumheld@gmail.com>
2020-10-15 10:55:09 -0500

    converted api call to axios

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,10 @@
+import axios from "axios";
 
 const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
-    const searchCall=function(search){var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&query="+search+"&page=1&include_adult=false";
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response){
+    const searchCall=function(search){
+    var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&query="+search+"&page=1&include_adult=false";
+            axios.get(queryURL)
+                .then(function(response){
                 console.log(response);
                 var moviesarray=response.results.map(arrfunc);
                 function arrfunc(mov){
@@ -22,10 +22,8 @@ const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
         
     const idCall=function(id){
         var queryURL="https://api.themoviedb.org/3/movie/"+id+"?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&append_to_response=credits,external_ids"
-        $.ajax({
-            url:queryURL,
-            method:"GET"
-        }).then(function(response){
+        axios.get(queryURL)
+        .then(function(response){
             console.log(response);
             var dirObj=response.credits.crew.filter(x=> x.department=="Directing");
             var cast="";
@@ -52,22 +50,29 @@ const APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
         });
     }
     const utellyCall=function(id){
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id="+id+"&source=tmdb",
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-                "x-rapidapi-key": "9b41398bccmsh054a4c1235dff30p1c1836jsn95542e375cf5"
-            }
-        }
         
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-            var utellyobj=response.collection.locations;
+        axios({
+            "method":"GET",
+            "url":"https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup",
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+            "x-rapidapi-key":"9b41398bccmsh054a4c1235dff30p1c1836jsn95542e375cf5",
+            "useQueryString":true
+            },"params":{
+            "country":"us",
+            "source_id":id,
+            "source":"tmdb"
+            }
+            })
+            .then((response)=>{
+                console.log(response)
+                var utellyobj=response.collection.locations;
             return(utellyobj);
-        });
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
 
     }
     module.exports={
